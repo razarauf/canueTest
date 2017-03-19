432a2c514de8b390c07a7401ff3c76d1ed86a6c0
razarauf <m.razarauf@gmail.com>
2017-03-18 19:58:47 -0700

    Update logic; Add documentation

--- solution/parse.py
+++ solution/parse.py
@@ -1,51 +1,116 @@
 #!/usr/bin/python
+"""
+Processes data from a CSV file, merges files, and outputs processed data results
+
+Author: Raza Rauf <m.razarauf@gmail.com>
+"""
 
 import sys, string, csv, numpy
 
-
 def printRows (listToPrint): 
 	""" 
-	print out all rows for test
+	prints out a 2D list
+
+	Parameters
+	----------
+	listToPrint: List
+		A 2D list
 	"""
 	for row in listToPrint:
 		print ', '.join(row)
 
 def readFile (filename):
+	""" 
+	reads data from a file into a list
+
+	Parameters
+	----------
+	filename: String
+		Name of the file to read
+
+	Returns
+	-------
+	list
+		a 2D list containing file data
+	"""
+
 	fileList = []
+	# open the file
 	with open (filename, 'rb') as csvfile: 
 		reader = csv.reader(csvfile)
+		# insert the csv file data into a list
 		fileList.extend(reader)
 	return fileList
 
-def parseList (filename, listToParse):
+def parseList (listToParse):
+	""" 
+	processes the inputted list
+
+	Parameters
+	----------
+	listToParse: List
+		The list to parse and process
+	Returns
+	-------
+	tuple
+		Tuple contains processed data
+	"""
+
+	# variables to various processed information
 	numOfValidRecords = 0
 	numOfNegRecords = 0
 	numOfMissing = 0
 	totalRecords = 0
 	aryOfValidRecords = []
 
+	# process each row individually
 	for row in listToParse:
+		# check if VOC is missing
 		if float(row[8]) == -999.0: 
 			row.append("3")
 			numOfMissing = numOfMissing+1
 			totalRecords = totalRecords+1
+		# check if VOC is negative
 		elif float(row[8]) < 0: 
 			row.append("2")
 			numOfNegRecords = numOfNegRecords+1
 			totalRecords = totalRecords+1
+		# otherwise VOC is valid
 		else: 
 			row.append("1")
 			numOfValidRecords = numOfValidRecords+1
 			totalRecords = totalRecords+1
+			# store the valid array to process for later
 			aryOfValidRecords.append(float(row[8]))
 
-	printSummaryTable (filename, numOfValidRecords, numOfNegRecords, numOfMissing, totalRecords, aryOfValidRecords)
+	return (numOfValidRecords, numOfNegRecords, numOfMissing, totalRecords, aryOfValidRecords)
 
 def printSummaryTable (filename, numOfValidRecords, numOfNegRecords, numOfMissing, totalRecords, aryOfValidRecords): 
+	""" 
+	processes inputted data further and prints out processed results
+
+	Parameters
+	----------
+	filename: String
+		Name of the file to write to 
+	numOfValidRecords: Int
+		Number of valid records
+	numOfNegRecords: Int
+		Number of negative records
+	numOfMissing: Int
+		Number of missing records 
+	totalRecords: Int
+		Total records 
+	aryOfValidRecords: List
+		List containing valid records
+	"""
+
+	# calculate percentage
 	percentOfValidRecords = (numOfValidRecords/float(totalRecords))*100
 	percentOfNegative = (numOfNegRecords/float(totalRecords))*100
 	percentOfMissing = (numOfMissing/float(totalRecords))*100
 
+	# process and print out formatted data
 	print "filename:" + filename
 	print 'Total records: ' + str(totalRecords)
 	print ('Number and percent of valid records %i, %.3f %%' % (numOfValidRecords, percentOfValidRecords))
@@ -60,36 +125,84 @@ def printSummaryTable (filename, numOfValidRecords, numOfNegRecords, numOfMissin
 	print 
 
 def writeToFile (listToWrite, filename): 
+	""" 
+	writes data from a list to a file using a helper function
+
+	Parameters
+	----------
+	listToWrite: List
+		a 2D list containing data to write to the file
+	filename: String
+		Name of the file to write to 
+	"""
+
+	# insert the header
 	listToWrite.insert (0, ['Date start:', 'Time start:', 'Date end:', 'Time end:','Time zone:', 'Trip ID','Latitude:','Longitude:','VOC_ppbv:', 'Data flag:'])
+	# call the helper funtion to process data further
 	fileWriterHelper (listToWrite, filename, False)
 
 
 def mergeFiles (listToWrite, filename, fileNumber): 
+	""" 
+	merges files together using a helper function
+
+	Parameters
+	----------
+	listToWrite: List
+		a 2D list containing data to write to the file
+	filename: String
+		Name of the file to write to 
+	fileNumber: int
+		Number of the file being merged
+	"""
+
+	# insert the header if this is the first set of data (first file) being merged
 	if fileNumber == 1: 
 		listToWrite.insert (0, ['Date start:', 'Time start:', 'Date end:', 'Time end:','Time zone:', 'Trip ID','Latitude:','Longitude:','VOC_ppbv:', 'Data flag:'])
 	fileWriterHelper (listToWrite, filename, True)
 	
 
 def fileWriterHelper (listToWrite, filename, append):
+	""" 
+	helper function for writing or appending to file
+
+	Parameters
+	----------
+	listToWrite: List
+		a 2D list containing data to write to the file
+	filename: String
+		Name of the file to write to 
+	append: Boolean
+		Flag for whether to append to the end of the file or not
+	"""
+
+	# check append flag to append to the file or just write out completely to the file
 	with open (filename, 'wb' if append==False else 'ab') as csvfile: 
 		writer = csv.writer(csvfile)
 		writer.writerows(listToWrite)
 
 def main (): 
+	# list of files to process
 	listOfFiles = ['VOC12015.csv', 'VOC42015.csv', 'VOC82015.csv']
+	
 	fileList = []
+	fileNumber = 0
+
+	# variables to various processed information
 	numOfValidRecords = 0
 	numOfNegRecords = 0
 	numOfMissing = 0
 	totalRecords = 0
-	fileNumber = 0
 	aryOfValidRecords = []
 
+	# process each file individually 
 	for eachFile in listOfFiles:
+		# counter for how many files have been processed
 		fileNumber = fileNumber + 1
 		fileList = readFile(eachFile)
-		
-		parseList (eachFile, fileList[1:])
+
+		(numOfValidRecords, numOfNegRecords, numOfMissing, totalRecords, aryOfValidRecords) = parseList (fileList[1:])
+		printSummaryTable (eachFile, numOfValidRecords, numOfNegRecords, numOfMissing, totalRecords, aryOfValidRecords)
 		writeToFile (fileList[1:], eachFile)
 		mergeFiles (fileList[1:], "mergedData.csv", fileNumber)
 
--- solution/runFolium.py
+++ solution/runFolium.py
@@ -1,25 +1,39 @@
 #!/usr/bin/python
+"""
+Takes the longitude and latitude information from a CSV file and exports mapped data to an HTML file
+
+Author: Raza Rauf <m.razarauf@gmail.com>
+"""
 
 import folium, datetime, parse
 
+# scale for various size of markers showing VOC level
 RADIUS_SCALE = 15.0;
 
 def main (): 
-
 	firstTime = True
+	# store a dictionary with key as day of the week and value as color
 	daysToColor = {"MONDAY": "#2ecc71", "TUESDAY":"#3498db", "WEDNESDAY":"#9b59b6", "THURSDAY":"#34495e", "FRIDAY":"#f1c40f", "SATURDAY":"#e67e22", "SUNDAY":"#e74c3c"}
 
+	# call the readFile funtion in the parse script
 	listToProcess = parse.readFile ('mergedData.csv')
 	for row in listToProcess[1:]: 
+		# if it is the first row -> set up the map 
+		# starting location is the longitude and latitude of the first row in the data
 		if firstTime:	
 			map_5 = folium.Map(location=[row[6], row[7]], zoom_start=13)
 			firstTime = False
 
-		if float(row[8]) != -999.0 and float(row[8]) > 0:  
+		# check whether the data is valid, column 9 was the flag for valid data
+		if int(row[9]) == 1:  
+			# temporarily store the date conversion to a day of the week 
 			tmpDayFromDate = datetime.datetime.strptime(row[0], '%d/%m/%Y').strftime('%A').upper()
+
+			# create the marker using the day of the week as a popup, color according to the day of the week
+			# stored in the dictionary, and radius (VOC level scaled by 15)
 			folium.RegularPolygonMarker(location=[row[6], row[7]], popup= tmpDayFromDate,
 			                   fill_color=daysToColor[tmpDayFromDate], number_of_sides=4, radius=float(row[8]) * RADIUS_SCALE).add_to(map_5)
-	
+	# exports mapped data to an HTML file
 	map_5.save('part3.html')
 
 # This is the standard boilerplate that calls the main() function.
